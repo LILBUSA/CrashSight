a7220a75df772c32133b286773a65940f0a44a22
Eye-apple <60296850+Eye-apple@users.noreply.github.com>
2022-11-28 14:23:16 -0500

    Add files via upload

---
+++ EngProject.java
@@ -0,0 +1,11 @@
+//import statements for io classes/functions and various utility classes/functions
+import java.io.*;
+import java.util.*;
+git remote add origin https://github.com/LILBUSA/CrashSight.git
+        git branch -M main
+        git push -u origin main
+public class EngProject {
+    public static void main (String [] args){
+        System.out.println("Simply get good.");
+    }
+}
