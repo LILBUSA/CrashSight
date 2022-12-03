83d0053a98562b5a22b813ec3937e156909a276c
Taylor <bigtay4eva@gmail.com>
2022-12-03 15:08:44 -0500

    asset files

--- src/com/CrashSight/data/Data.java
+++ src/com/CrashSight/data/Data.java
@@ -6,7 +6,7 @@ import org.apache.poi.ss.usermodel.CellType;
 import java.io.IOException;
 
 public class Data {
-    private final ExcelReader reader = new ExcelReader("CrashSight\\assets\\housing_data_range.xlsx"); // finished xlsx file will be stored here
+    private final ExcelReader reader = new ExcelReader("CrashSight\\assets\\Housing_Data.xlsx"); // finished xlsx file will be stored here
     private DataSet delinquent_mortgage, household_debt, leverage, subprime_mortgage, special_loan, builders_sentiment, housing_inventory, mortgage_rates, difference_housingprices_rent; // all current variables expected be used
     private DataSet[] sets = new DataSet[] {delinquent_mortgage, household_debt, leverage, subprime_mortgage, special_loan, builders_sentiment, housing_inventory, mortgage_rates, difference_housingprices_rent};
 
@@ -16,8 +16,6 @@ public class Data {
 
     public DataSet[] updateData(DataSet[] sets) {
         for (int i = 0; i < reader.wb.getNumberOfSheets(); i++) {
-            int row = 0;
-
             for (int r = 1; i < reader.sheet.getPhysicalNumberOfRows(); i++) {
                 sets[i].addPoint(new DataPoint((int)reader.readNumericCellData(r, 0), (int)reader.readNumericCellData(r, 1), reader.readNumericCellData(r, 2)));
 //                Cell cell = reader.readCellData(r, 0);
